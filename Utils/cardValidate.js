ab56ef1edb49480105ef0a9120b0c3ab7e103aff
Ahmed Ali <ahmed.balti@safarifoneict.com>
2023-02-24 17:04:29 +0500

    Friday tasks

--- Utils/cardValidate.js
+++ Utils/cardValidate.js
@@ -2,7 +2,7 @@ module.exports.cardValidation = (card, pin, expiry) => {
   if (!/^(?:[0-9]{11}|[0-9]{2}-[0-9]{3}-[0-9]{6})$/.test(card)) {
     return { isValid: false, message: "not valid account number" };
   }
-  if (pin != 4 && isNaN(pin))
+  if (pin != 4 || isNaN(pin))
     return { isValid: false, message: "pin not valid" };
   var exp = new Date(expiry);
   if (exp < Date.now()) return { isValid: false, message: "expired card" };
